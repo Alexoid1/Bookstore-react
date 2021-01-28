676e3438e0f0fe7b38c096b5342337402de4f445
Alexoid1 <alex_zam@hotmail.es>
2021-01-27 23:57:55 -0500

    add github actions

--- src/actions/action-types.js
+++
@@ -1,2 +0,0 @@
-export const REMOVE_BOOK = 'REMOVE_BOOK';
-export const CREATE_BOOK = 'CREATE_BOOK';
--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,16 @@
 import './App.css';
-import BooksForm from './BooksForm';
-import BooksList from './BooksList';
+import BooksForm from '../containers/BooksForm';
+import BooksList from '../containers/BooksList';
 
 function App() {
   return (
-    <div className="App">
-      <BooksForm />
-      <BooksList />
-    </div>
+    <>
+      <div className="App">
+        <BooksForm />
+        <BooksList />
+      </div>
+    </>
+    
   );
 }
 
