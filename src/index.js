4fe96ba5a26ef8ee89901e3c0b5441fe40f28af7
Alexoid1 <alex_zam@hotmail.es>
2021-01-28 17:22:20 -0500

    form change state

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import { Provider } from 'react-redux';
 import App from './components/App';
 import store from './reducers/index';
